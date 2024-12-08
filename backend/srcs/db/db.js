7297dec17007ba0c6d7e8a2e03b7c505e2405f26
ahmadrazanawab <ahmadrazanawab@gmail.com>
2024-12-08 12:45:23 +0530

    mongodb url add

--- backend/srcs/db/db.js
+++ backend/srcs/db/db.js
@@ -1,34 +1,19 @@
 const mongoose = require('mongoose');
-// const { DB_NAME } = require('../constants');
-const MONGODB_URL = "mongodb://127.0.0.1:27017/quizMaster"
+const { DB_NAME } = require('../constants');
+// const MONGODB_URL = "mongodb://127.0.0.1:27017/quizMaster"
 
-// const MONGODB_URL = "mongodb+srv://ahmadrazanawab:ahmad123@cluster0.xuqma.mongodb.net/"
+const MONGODB_URI = "mongodb+srv://ahmadrazanawab:heVoTlMSm9Q7Qxky@cluster0.xuqma.mongodb.net"
 
-mongoose.set('strictQuery', true);
-const connetedToMongodb = async (req,res) => {
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
     try {
-        const mongodb = mongoose.connect(MONGODB_URL, () => {
-            console.log("Connection is Successfull");
-        });
-        // console.log(mongodb.connection.host);
+        const connectionInstance = await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
-        console.log("Connection Error...", error);
-        res.status(501).json("Something is wrong");
+        console.log("MONGODB connection error", error);
+        process.exit(1);
     }
 }
 
 
-module.exports = connetedToMongodb;
-
-
-// const connectDB = async () => {
-//     try {
-//         const connectionInstance = await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
-//         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
-//     } catch (error) {
-//         console.log("MONGODB connection error ", error);
-//         process.exit(1);
-//     }
-// }
-
-// export default connectDB
+module.exports = connectDB;
--- backend/srcs/index.js
+++ backend/srcs/index.js
@@ -3,10 +3,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require('body-parser');
-const connetedToMongodb = require('./db/db');
+const connectDB = require('./db/db');
 
 const port = process.env.PORT || 8000
-connetedToMongodb();
+// connetedToMongodb();
+    connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port:${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("MONGO db connection failed !!! ",error)
+    })
+
+
 app.use(bodyParser.json());
 app.use(express.json());
 
@@ -14,16 +25,16 @@ dotenv.config({
     path: './.env'
 })
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+
 app.get('/',(req, res) => {
     res.send("hello worlds 2");
 })
 
 app.use('/api/c1/', require('./routes/Category.route'));
 app.use('/api/a1/', require('./routes/Answer.route'));
-
-
-app.listen(port,()=> {
-    console.log(`Server is started at http://localhost:${port}`);
-})
 
